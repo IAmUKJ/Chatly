20292ac085fb137c5a77352a52c357e462511255
Utkarsh kumar Jha <iamukj013@gmail.com>
2025-06-26 03:02:26 +0530

    Production Ready

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../components/Sidebar'
+import Sidebar from '../components/SideBar'
 import MessageArea from '../components/MessageArea'
 import { useSelector } from 'react-redux'
 import getMessage from '../customHooks/getMessages'
